67f7954559a304ca5a04ff428e96fbe30e2dd792
elijah <elijah@riseup.net>
2020-10-01 23:10:59 -0700

    pull in list of apps from git repo

--- tasks/update-app-list.rb
+++ tasks/update-app-list.rb
@@ -1,3 +1,52 @@
+module Apps
+  APPS_URL  = "https://gitlab.com/calyxos/platform_prebuilts_calyx/-/raw/android10/fdroid/repo/index.xml"
+  HOME      = File.expand_path('../..', __FILE__)
+  TMP_FILE  = "#{HOME}/tmp/apps.xml"
+  DEST_FILE = "#{HOME}/pages/_data/apps.yml"
+  ICON_DIR  = "#{HOME}/pages/assets/images/apps"
+
+  class << self
+    def requirements_met?
+      require "rexml/document"
+    rescue LoadError
+      return false
+    end
+
+    def update
+      # gitlab does not send Last-Modified, so --timestamping has no effect
+      system("wget", "--timestamping", APPS_URL, "-O", TMP_FILE)
+
+      return unless File.exist?(TMP_FILE)
+
+      file = File.new(TMP_FILE)
+      xml = REXML::Document.new file
+      apps = {}
+      xml.root.each_element("application") do |app_el|
+        app = {}
+        id = app_el["id"]
+        ["icon", "web", "desc", "name", "summary"].each do |el_name|
+          app[el_name] = app_el.elements[el_name].text.to_s.strip
+        end
+        apps[id] = app
+      end
+
+      File.open(DEST_FILE, 'w') do |f|
+        f.write apps.to_yaml
+      end
+      puts "CREATED #{DEST_FILE}"
+      puts "DONE"
+    end
+  end
+end
+
+
 desc "Update the file pages/_data/apps.yml"
 task "update-apps" do
+  if Apps.requirements_met?
+    Apps.update
+  else
+    puts "Requirements:"
+    puts "apt install wget"
+    puts "gem install rexml"
+  end
 end
--- tasks/update-releases.rb
+++ tasks/update-releases.rb
@@ -3,115 +3,124 @@
 require 'fileutils'
 require 'yaml'
 
-RELEASES        = ["stable", "beta"]
-RELEASE_SOURCE  = 'https://gitlab.com/calyxos/release.git'
-HASHES_SOURCE   = 'https://gitlab.com/calyxos/releases.git'
-RELEASE_DL_BASE = 'https://release.calyxinstitute.org/'
-HOME            = File.expand_path('../..', __FILE__)
-RELEASE_CACHE   = "#{HOME}/tmp/release"
-HASHES_CACHE    = "#{HOME}/tmp/hashes"
-DEST_FILE       = "#{HOME}/pages/_data/downloads.yml"
+module Releases
 
-CODENAME_MAP = {
-  "walleye"        => "Pixel 2",
-  "taimen"         => "Pixel 2 XL",
-  "blueline"       => "Pixel 3",
-  "crosshatch"     => "Pixel 3 XL",
-  "sargo"          => "Pixel 3a",
-  "bonito"         => "Pixel 3a XL",
-  "flame"          => "Pixel 4",
-  "coral"          => "Pixel 4 XL",
-  "sunfish"        => "Pixel 4a",
-  "jasmine_sprout" => "Xiaomi Mi A2"
-}
+  RELEASES        = ["stable", "beta"]
+  RELEASE_SOURCE  = 'https://gitlab.com/calyxos/release.git/'
+  HASHES_SOURCE   = 'https://gitlab.com/calyxos/releases.git/'
+  RELEASE_DL_BASE = 'https://release.calyxinstitute.org/'
+  HOME            = File.expand_path('../..', __FILE__)
+  RELEASE_CACHE   = "#{HOME}/tmp/release"
+  HASHES_CACHE    = "#{HOME}/tmp/hashes"
+  DEST_FILE       = "#{HOME}/pages/_data/downloads.yml"
 
-def clone_release_repo
-  FileUtils.mkdir_p(RELEASE_CACHE)
-  Dir.chdir(RELEASE_CACHE) do
-    `git clone #{RELEASE_SOURCE} .`
-  end
-end
+  CODENAME_MAP = {
+    "walleye"        => "Pixel 2",
+    "taimen"         => "Pixel 2 XL",
+    "blueline"       => "Pixel 3",
+    "crosshatch"     => "Pixel 3 XL",
+    "sargo"          => "Pixel 3a",
+    "bonito"         => "Pixel 3a XL",
+    "flame"          => "Pixel 4",
+    "coral"          => "Pixel 4 XL",
+    "sunfish"        => "Pixel 4a",
+    "jasmine_sprout" => "Xiaomi Mi A2"
+  }
 
-def clone_hashes_repo
-  FileUtils.mkdir_p(HASHES_CACHE)
-  Dir.chdir(HASHES_CACHE) do
-    `git clone #{HASHES_SOURCE} .`
-  end
-end
-
-def update_release_repo
-  Dir.chdir(RELEASE_CACHE) do
-    `git pull`
-  end
-end
-
-def update_hashes_repo
-  Dir.chdir(HASHES_CACHE) do
-    `git pull`
-  end
-end
-
-def parse_releases
-  releases = {}
-  RELEASES.each do |release|
-    releases[release] = parse_release(release)
-  end
-  return releases
-end
-
-def get_hash_for(filename)
-  filename += ".sha256sum"
-  Dir.chdir(HASHES_CACHE) do
-    if File.exist?(filename)
-      return File.read(filename).strip
-    else
-      return nil
-    end
-  end
-end
-
-def parse_release(release)
-  info = []
-  Dir.chdir(RELEASE_CACHE) do
-    CODENAME_MAP.each do |codename, device|
-      release_filename = codename + "-" + release
-      unless File.exist?(release_filename)
-        puts "SKIPPED #{codename}"
-        next
+  class << self
+    def clone_release_repo
+      FileUtils.mkdir_p(RELEASE_CACHE)
+      Dir.chdir(RELEASE_CACHE) do
+        `git clone #{RELEASE_SOURCE} .`
       end
-      release_file = File.read(release_filename)
-      build_number, timestamp, build_id = release_file.split(' ')
-      date = build_number.split('.')[0..2].join('-')
-      firmware_filename = codename + "-factory-#{build_number}.zip"
-      sha256 = get_hash_for(firmware_filename)
-      info << {
-        "name" => device,
-        "codename" => codename,
-        "link" => RELEASE_DL_BASE + firmware_filename,
-        "sha256" => sha256,
-        "date" => date
-      }
-      puts "FINISHED #{codename}"
+    end
+
+    def clone_hashes_repo
+      FileUtils.mkdir_p(HASHES_CACHE)
+      Dir.chdir(HASHES_CACHE) do
+        `git clone #{HASHES_SOURCE} .`
+      end
+    end
+
+    def update_release_repo
+      Dir.chdir(RELEASE_CACHE) do
+        `git pull`
+      end
+    end
+
+    def update_hashes_repo
+      Dir.chdir(HASHES_CACHE) do
+        `git pull`
+      end
+    end
+
+    def parse_releases
+      releases = {}
+      RELEASES.each do |release|
+        releases[release] = parse_release(release)
+      end
+      return releases
+    end
+
+    def get_hash_for(filename)
+      filename += ".sha256sum"
+      Dir.chdir(HASHES_CACHE) do
+        if File.exist?(filename)
+          return File.read(filename).strip
+        else
+          return nil
+        end
+      end
+    end
+
+    def parse_release(release)
+      info = []
+      Dir.chdir(RELEASE_CACHE) do
+        CODENAME_MAP.each do |codename, device|
+          release_filename = codename + "-" + release
+          unless File.exist?(release_filename)
+            puts "SKIPPED #{codename}"
+            next
+          end
+          release_file = File.read(release_filename)
+          build_number, timestamp, build_id = release_file.split(' ')
+          date = build_number.split('.')[0..2].join('-')
+          firmware_filename = codename + "-factory-#{build_number}.zip"
+          sha256 = get_hash_for(firmware_filename)
+          info << {
+            "name" => device,
+            "codename" => codename,
+            "link" => RELEASE_DL_BASE + firmware_filename,
+            "sha256" => sha256,
+            "date" => date
+          }
+          puts "FINISHED #{codename}"
+        end
+      end
+      return info
+    end
+
+    def update
+      unless File.exist?(RELEASE_CACHE)
+        clone_release_repo
+      end
+      unless File.exist?(HASHES_CACHE)
+        clone_hashes_repo
+      end
+      update_release_repo
+      update_hashes_repo
+      releases = parse_releases
+      releases["releases"] = RELEASES
+      File.open(DEST_FILE, 'w') do |f|
+        f.write releases.to_yaml
+      end
+      puts "DONE"
+      exit
     end
   end
-  return info
 end
 
 desc 'fetch data on current firmware releases and build pages/_data/downloads.yml file'
 task 'update-releases' do
-  unless File.exist?(RELEASE_CACHE)
-    clone_release_repo
-  end
-  unless File.exist?(HASHES_CACHE)
-    clone_hashes_repo
-  end
-  update_release_repo
-  update_hashes_repo
-  releases = parse_releases
-  releases["releases"] = RELEASES
-  File.open(DEST_FILE, 'w') do |f|
-    f.write releases.to_yaml
-  end
-  puts "DONE"
-  exit
+  Releases.update
 end
