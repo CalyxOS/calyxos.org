e4ad4e3e411c0683d8cb399e76445ee2dd155817
Chirayu Desai <chirayudesai1@gmail.com>
2021-07-19 20:56:24 +0530

    update-app-list: Rewrite icon handling, support using full list
    * Not the cleanest code but it works
    * Can clean it up when we switch to index-v1.json, or maybe we can
      just use that directly instead, potentially grabbing code from
      fdroid-website to parse some of that
    
    Change-Id: I775c71f749a142c62aa7165583ff3f727e7aa60d

--- tasks/update-app-list.rb
+++ tasks/update-app-list.rb
@@ -1,6 +1,6 @@
 module Apps
-  APPS_URL  = "https://gitlab.com/CalyxOS/platform_prebuilts_calyx_fdroid/-/raw/android11-qpr1/repo/index.xml"
-  ICON_URL  = "https://gitlab.com/CalyxOS/platform_prebuilts_calyx_fdroid/-/raw/android11-qpr1/repo/icons-640/"
+  APPS_URL  = "https://gitlab.com/CalyxOS/platform_prebuilts_calyx_fdroid/-/raw/3e5701c25b2f4acbbdcda81f70beec4e0f504f0e/repo/index.xml"
+  ICON_URL  = "https://gitlab.com/CalyxOS/platform_prebuilts_calyx_fdroid/-/raw/3e5701c25b2f4acbbdcda81f70beec4e0f504f0e/repo/"
   HOME      = File.expand_path('../..', __FILE__)
   TMP_FILE  = "#{HOME}/tmp/apps.xml"
   DEST_FILE = "#{HOME}/pages/_data/apps.yml"
@@ -26,19 +26,25 @@ module Apps
       file = File.new(TMP_FILE)
       xml = REXML::Document.new file
       apps = {}
+      inner_apps = []
       xml.root.each_element("application") do |app_el|
         app = {}
         id = app_el["id"]
-        ["icon", "web", "desc", "name", "summary"].each do |el_name|
+        ["web", "desc", "name", "summary"].each do |el_name|
           element = app_el.elements[el_name]
           value = element.nil? ? "" : element.text.to_s.strip
           app[el_name] = value
-          if el_name == "icon"
-            system("wget", "--timestamping", ICON_URL + value, "-O", ICON_DIR + value)
-          end
         end
-        apps[id] = app
+        system("wget", "--timestamping", ICON_URL + id + "/en-US/icon.png", "-O", ICON_DIR + id + ".png")
+        inner_apps << {
+          "pkgname" => id,
+          "web" => app["web"],
+          "desc" => app["desc"],
+          "name" => app["name"],
+          "summary" => app["summary"]
+        }
       end
+      apps["apps"] = inner_apps
 
       File.open(DEST_FILE, 'w') do |f|
         f.write apps.to_yaml
