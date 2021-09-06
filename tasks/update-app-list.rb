56d4f24e9cccab887ab5084df9d0eccfdd9eb62f
elijah <elijah@riseup.net>
2021-09-06 10:42:54 -0700

    task update-apps: switch to json source and generate app pages using a template.
    Change-Id: I979e1347982828fd4977e3c10143b42a9c5b337c

--- tasks/update-app-list.rb
+++ tasks/update-app-list.rb
@@ -1,53 +1,92 @@
 module Apps
-  APPS_URL  = "https://gitlab.com/CalyxOS/platform_prebuilts_calyx_fdroid/-/raw/android11-qpr1/repo/index.xml"
+
+  APPS_URL  = "https://gitlab.com/CalyxOS/platform_prebuilts_calyx_fdroid/-/raw/android11-qpr1/repo/index-v1.json"
   ICON_URL  = "https://gitlab.com/CalyxOS/platform_prebuilts_calyx_fdroid/-/raw/android11-qpr1/repo/"
   HOME      = File.expand_path('../..', __FILE__)
-  TMP_FILE  = "#{HOME}/tmp/apps.xml"
+  TMP_FILE  = "#{HOME}/tmp/apps.json"
   DEST_FILE = "#{HOME}/pages/_data/apps.yml"
   ICON_DIR  = "#{HOME}/pages/assets/images/apps/"
+  APP_PAGES = "#{HOME}/pages/docs/guide/apps/"
+  TEMPLATE  = "#{HOME}/pages/docs/guide/apps/_template.md"
+
+  ELEMENTS  = %w(packageName webSite description name summary icon license categories sourceCode donate issueTracker)
 
   class << self
     def requirements_met?
-      require "rexml/document"
+      require "liquid"
     rescue LoadError
       return false
     end
 
-    def update
-      # gitlab does not send Last-Modified, so --timestamping has no effect
-      system("wget", "--timestamping", APPS_URL, "-O", TMP_FILE)
-
-      return unless File.exist?(TMP_FILE)
-
-      # Clear old icons
-      FileUtils.rm_rf(ICON_DIR)
-      FileUtils.mkdir(ICON_DIR)
-
-      file = File.new(TMP_FILE)
-      xml = REXML::Document.new file
-      apps = {}
-      inner_apps = []
-      xml.root.each_element("application") do |app_el|
-        app = {}
-        id = app_el["id"]
-        ["web", "desc", "name", "summary"].each do |el_name|
-          element = app_el.elements[el_name]
-          value = element.nil? ? "" : element.text.to_s.strip
-          app[el_name] = value
-        end
-        system("wget", "--timestamping", ICON_URL + id + "/en-US/icon.png", "-O", ICON_DIR + id + ".png")
-        inner_apps << {
-          "pkgname" => id,
-          "web" => app["web"],
-          "desc" => app["desc"],
-          "name" => app["name"],
-          "summary" => app["summary"]
-        }
+    def download_icon(app)
+      id = app["packageName"]
+      src_url = ICON_URL + id + "/en-US/icon.png"
+      dest_file = ICON_DIR + id + ".png"
+      unless File.exist?(dest_file)
+        system("wget", src_url, "-O", dest_file)
       end
-      apps["apps"] = inner_apps
+    end
+
+    def render_app_page(app, template)
+      File.open(APP_PAGES + app["slug"] + ".md", 'w') do |f|
+        f.write template.render(app)
+      end
+    end
+
+    # the markup used in f-droid descriptions is mostly html, some markup, but
+    # has these weird ''quote'' markdup that I think is supposed to be bold.
+    def to_markdown(str)
+      str.gsub(/'''(.*)'''/, "**\\1**\n").gsub(/''(.*)''/, "**\\1**")
+    end
+
+    def fetch(hsh, locator)
+      locator.split('.').each do |i|
+        hsh = hsh[i]
+        return nil if hsh.nil?
+      end
+      return hsh
+    end
+
+    def copy_elements(app)
+      new_app = {}
+      ELEMENTS.each do |el_name|
+        value = app[el_name]
+        value = value.nil? ? "" : value
+        new_app[el_name] = value
+      end
+      #p 'x' *1000
+      #p app
+      if new_app["name"] == ""
+        new_app["name"] = fetch(app, "localized.en-US.name")
+      end
+      if new_app["summary"] == ""
+        new_app["summary"] = fetch(app, "localized.en-US.summary")
+      end
+      new_app["iconUrl"] = '/assets/images/apps/' + new_app["packageName"] + '.png'
+      new_app["slug"] = new_app["name"].downcase.gsub(/[^a-z0-9]/i,'-').gsub(/\-\-/,'-')
+      new_app["description"] = to_markdown(new_app["description"])
+      return new_app
+    end
+
+    def update
+      FileUtils.mkdir_p(File.dirname(TMP_FILE))
+      system("wget", APPS_URL, "-O", TMP_FILE)
+      return unless File.exist?(TMP_FILE)
+      FileUtils.mkdir_p(ICON_DIR)
+
+      template = Liquid::Template.parse(File.read(TEMPLATE))
+      json = JSON.load(File.new(TMP_FILE))
+
+      yml = {"apps" => []}
+      json["apps"].each do |app|
+        app = copy_elements(app)
+        download_icon(app)
+        render_app_page(app, template)
+        yml["apps"] << app
+      end
 
       File.open(DEST_FILE, 'w') do |f|
-        f.write apps.to_yaml
+        f.write yml.to_yaml
       end
       puts "CREATED #{DEST_FILE}"
       puts "DONE"
@@ -63,6 +102,6 @@ task "update-apps" do
   else
     puts "Requirements:"
     puts "apt install wget"
-    puts "gem install rexml"
+    puts "gem install liquid"
   end
 end
