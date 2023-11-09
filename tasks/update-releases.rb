05288b2a67e2a9064cdb9eb894a0806c13e6a51c
Chirayu Desai <chirayudesai1@gmail.com>
2023-11-10 03:02:11 +0530

    tasks: include security express in releases too
    Change-Id: I88ba66c00ca01fbba471c8bc0a4d81a0c2553558

--- tasks/update-releases.rb
+++ tasks/update-releases.rb
@@ -5,7 +5,7 @@ require 'yaml'
 
 module Releases
 
-  RELEASES        = ["stable", "beta"]
+  RELEASES        = ["stable", "beta", "security-express"]
   RELEASE_SOURCE  = 'https://review.calyxos.org/CalyxOS/release'
   HASHES_SOURCE   = 'https://review.calyxos.org/CalyxOS/releases'
   RELEASE_DL_BASE = 'https://release.calyxinstitute.org/'
