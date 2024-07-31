7c2648977b5f544def5828a63fdeaf0232f5d103
Not Root <notroot@localhost>
2024-07-31 20:47:47 +0000

    Build website
    Commits:
    70354c6 (gitlab/main) about: fix Typo
    05232b7 5.9.0-2: more devices
    8590dcb FP4: Downloads temporarily removed
    7553ded devon,rhode: Downloads temporarily removed
    
    Change-Id: I2b38e6bc325b30eba4377f1b864af76bfeb0d997

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1722027424.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1722458821.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1722027424.json";
+    "/assets/sitecontents-1722458821.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
