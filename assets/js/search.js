ff088be8fe95e0d7ba5ddb06925600502755fae9
Not Root <notroot@localhost>
2024-09-06 16:13:00 +0000

    Build website
    a242035 (HEAD, gitlab/main, gitlab/HEAD) posts: 5.11.0
    893bfc0 FP5: Temporarily remove downloads
    a7893b6 fixup! posts: 5.10.x, 5.10.3 is live
    f15e838 posts: 5.10.x, 5.10.3 is live
    8398c11 build: repo init --git-lfs
    3396ea0 fixup! posts: 5.10.x
    217944a posts: 5.10.x
    8ef4558 build: update requirements
    a890642 Add upcoming Pixel devices
    68e1b62 posts: 5.10.0
    bc8c820 Redirect calyxos.org/start to getting started
    
    Change-Id: I9f42cea02b2adbf17d1334f924876ad3aea42463

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1722458821.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1725639124.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1722458821.json";
+    "/assets/sitecontents-1725639124.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
