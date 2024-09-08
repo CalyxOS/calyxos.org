a3b9777eb25cdaff7260f6903e3ff51dc8fff480
Chirayu Desai <chirayudesai1@gmail.com>
2024-09-09 03:44:00 +0530

    Build website
    58506d3 (HEAD, gitlab/main, gerrit/main) posts: 5.11.1
    c33e90b Update download links for everything
    92f97a8 Add known working keyserver to gpg recv-keys
    4cd1381 device-flasher: 1.0.10
    592879a Reland "Bump ruby & gems to latest stable releases"
    
    Change-Id: Idd72b7f0543bff775f2fc4f00132814a220d2668

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1725639124.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1725833628.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1725639124.json";
+    "/assets/sitecontents-1725833628.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
