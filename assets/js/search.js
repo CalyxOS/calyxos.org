bce97ab6575b1507669b1d04d089bbc842ff5826
Chirayu Desai <chirayudesai1@gmail.com>
2024-10-04 03:31:02 +0530

    Build
    568fe220 (HEAD, gitlab/main) FP4: Temporarily remove downloads
    e0bf1175 posts: Mention 5.11.2 rollout halting
    
    Change-Id: I60915b100ee156809f0ed7cf1d0c339c37ff776f

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1727272721.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1727992859.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1727272721.json";
+    "/assets/sitecontents-1727992859.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
