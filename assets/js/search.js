db904e785c62125891e1fe16635d0f0bfebdbccc
Chirayu Desai <chirayudesai1@gmail.com>
2024-10-05 04:44:05 +0530

    Build
    1619421f (HEAD, gitlab/main, gerrit/main) FP4: 5.11.3 factory, for rollback
    c4db1e6d downloads: 5.11.3 beta and security-express6
    4f4ef61f posts: 5.11.3
    
    Change-Id: I41bbb56e2a4908ca5f25feae328eeb6fe5af642a

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1727992859.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1728083621.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1727992859.json";
+    "/assets/sitecontents-1728083621.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
