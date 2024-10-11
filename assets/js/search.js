a7ddaaa4e2102d2d9fc6af7b15201593ed1cc1e5
Chirayu Desai <chirayudesai1@gmail.com>
2024-10-12 03:03:32 +0530

    Build
    e71d15ea (HEAD, gitlab/main) downloads: 5.12.1 security-express6
    55c881c3 posts: 5.12.1
    
    Change-Id: Ibdb38eb677079b2c7fc6109546e4f79770941e72

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1728676899.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1728682402.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1728676899.json";
+    "/assets/sitecontents-1728682402.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
