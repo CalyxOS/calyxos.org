e7ec9cd2aa2a5f7d14000f63d17e063b7d945ae6
Chirayu Desai <chirayu@calyxinstitute.org>
2024-11-20 20:21:18 +0000

    Revert "Build: downloads, install: 6.1.0"
    This reverts commit 311901c09a909fc91a57190fcf66c6d76581375c.
    
    Reason for revert: Reland once Wi-Fi calling is fixed.
    
    Change-Id: Ia5fd28cf02993ff3ef69ebed6ead82f1fded7bd6

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1732116332.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1731791538.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1732116332.json";
+    "/assets/sitecontents-1731791538.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
