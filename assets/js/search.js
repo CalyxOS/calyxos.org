c559bbadf1c1710ed0e420f1a773a3ce6be5d38f
Chirayu Desai <chirayudesai1@gmail.com>
2024-11-30 19:57:10 +0530

    Build: Pull FP factory
    * https://forum.fairphone.com/t/software-update-fp5-ut2e-b-078-20241105/113711
    * https://forum.fairphone.com/t/software-update-fp4-tp2n-c-0128-20241030/113717
    
    Change-Id: Ieec58f482d2f548fe7a8a7abe303061e1c09aeb8

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1732663398.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1732976813.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1732663398.json";
+    "/assets/sitecontents-1732976813.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
