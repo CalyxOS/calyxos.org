c5a3c44a5ce257c1b399232eec62b02116540a2e
Chirayu Desai <chirayudesai1@gmail.com>
2024-12-01 00:20:35 +0530

    Build
    dfbe1575 (HEAD, gitlab/main) 5.13.0-2: FP factory
    365fe8c4 Add Pixel 9 series to various pages
    
    Change-Id: I64f8457ecf07fd2eaf5792427760fc7000d60095

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1732976813.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1732992616.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1732976813.json";
+    "/assets/sitecontents-1732992616.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
