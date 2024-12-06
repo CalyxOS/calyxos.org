076e512e49ec9a7fdadd304c3815a8a7611cefeb
Chirayu Desai <chirayudesai1@gmail.com>
2024-12-06 19:57:58 +0530

    Build
    a12a6d09 (HEAD) downloads: 6.2.0 beta & security express
    e5bd221d (gitlab/main) posts: 6.2.0
    c568f7a1 Pull G32, G52 builds: Rollback
    a0ba708c Update device support dates
    
    Change-Id: Ia3db93abd908acd0a561041dc4b54581edf3452f

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1733419348.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1733495266.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1733419348.json";
+    "/assets/sitecontents-1733495266.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
