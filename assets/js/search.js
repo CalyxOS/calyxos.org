2edb25602f315db7bed9434dbf515a3f7cd2dd37
Chirayu Desai <chirayudesai1@gmail.com>
2024-12-24 21:18:47 +0530

    Build: 6.2.1-2: factory
    Change-Id: Ie343428e824c3fdddf1eecc61ca7dff4f1fa5977

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1734531953.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1735055324.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1734531953.json";
+    "/assets/sitecontents-1735055324.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
