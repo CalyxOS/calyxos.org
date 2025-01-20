e4840f2c655c631cad979e4a3e500556a8003f50
Chirayu Desai <chirayudesai1@gmail.com>
2025-01-20 17:21:04 +0530

    Build
    cb31ced2 (HEAD) install: 6.3.0
    b929f840 downloads: 6.3.0 stable
    a317b49c (gitlab/main, gitlab/HEAD) docs: development: Change current branch mentions to android15-qpr1
    
    Change-Id: I91d10028dda9a063d7580d83eded81a4c6d63386

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1736377647.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1737373798.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1736377647.json";
+    "/assets/sitecontents-1737373798.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
