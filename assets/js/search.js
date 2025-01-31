ac9435931cf24ec6227bca4ea414c3e53872dee0
Chirayu Desai <chirayudesai1@gmail.com>
2025-01-31 15:47:30 +0530

    Build: device-support: Android 15
    Change-Id: I6825dc82dee53994efa040fa725e732728ef08a5

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1738316784.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1738318629.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1738316784.json";
+    "/assets/sitecontents-1738318629.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
