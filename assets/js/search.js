49756e211ff3801785583779575df1b9e4baf212
Chirayu Desai <chirayudesai1@gmail.com>
2025-02-11 22:57:18 +0530

    Build
    d62d4d08 (HEAD, gitlab/main, gitlab/HEAD) timely release but late post for 6.4.0
    4b8bc03d install: 6.4.0
    d84323b3 downloads: 6.4.0
    
    Change-Id: I8331377aa96514facf867983fadd117942157d10

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1738675322.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1739294831.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1738675322.json";
+    "/assets/sitecontents-1739294831.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
