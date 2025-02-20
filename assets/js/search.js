d2cd176a3e3a156a04a41e19a8eed9b5ea97497a
Chirayu Desai <chirayudesai1@gmail.com>
2025-02-21 03:03:04 +0530

    Build: fixup! posts: FP5 restrict-usb
    Change-Id: I180be6f2ee112a49ce7d26a8364eedc4d02af7f4

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1740084803.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1740087179.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1740084803.json";
+    "/assets/sitecontents-1740087179.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
