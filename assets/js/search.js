165e827fd8fea24484eee000ac63cf1e9a48a921
Chirayu Desai <chirayudesai1@gmail.com>
2025-02-24 21:32:02 +0530

    Build
    0fd211c3 (HEAD, gitlab/main, gitlab/HEAD) fixup! ota: Don't link to factory, doesn't make sense
    557f204e fixup! posts: 6.4.2, /global-no-cleartext/
    d8842fb0 fixup! Minimal Android Auto page
    f20ef807 downloads: 6.4.2 security-express7, and no fifteen
    e3d8c11a ota: Don't link to factory, doesn't make sense
    
    Change-Id: I2e6ca270ec958532512e74300f03c7eef020207a

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1740268043.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1740412899.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1740268043.json";
+    "/assets/sitecontents-1740412899.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
