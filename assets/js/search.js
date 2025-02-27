cafbd07fde8fe28c0d48a79d25e4c0f0a6312085
Chirayu Desai <chirayudesai1@gmail.com>
2025-02-28 04:09:47 +0530

    Build
    ec9b5d09 (HEAD, gitlab/main, gitlab/HEAD, gerrit/main, gerrit/HEAD) install: 6.4.2
    cb2efcc2 downloads: 6.4.2 stable, factory
    62841067 fixup! posts: 6.4.2, /global-no-cleartext/
    
    Change-Id: If216340fcf7de0ed3ddc2b31d19081e7a66de296

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1740694684.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1740695974.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1740694684.json";
+    "/assets/sitecontents-1740695974.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
