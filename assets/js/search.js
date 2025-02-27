8f934813fc7144c5170618835a5a602bb7f7702a
Chirayu Desai <chirayudesai1@gmail.com>
2025-02-28 03:48:23 +0530

    Build
    cca61db3 (HEAD, gitlab/main, gitlab/HEAD, gerrit/main, gerrit/HEAD) rake generate-data-pages
    9343e550 install: Mention web installer on second page
    58d497db install: Mention web installer on first page
    aff83ac6 Location privacy: remove Fake Traveler
    0089e9f2 Bootloader lock: be clearer about the consequences of relocking and explain device-flasher
    
    Change-Id: I3d1fe04d61c1fb1a2bd7e3e02f0f070ab4f1b98a

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1740412899.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1740694684.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1740412899.json";
+    "/assets/sitecontents-1740694684.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
