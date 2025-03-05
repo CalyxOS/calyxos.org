70bfd83bfbbd218e5a6de58f5cfdeb71f166f091
Chirayu Desai <chirayudesai1@gmail.com>
2025-03-06 03:11:51 +0530

    Build
    fe422f0b (HEAD) hawao: Pull build, avb rollback index, stock update
    990e9282 (gitlab/main, gitlab/HEAD) Explain version 0.0.0
    
    Change-Id: I4a9bbecd939498ec16ec0dfea4f3142d4d2057f2

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1740695974.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1741210889.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1740695974.json";
+    "/assets/sitecontents-1741210889.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
