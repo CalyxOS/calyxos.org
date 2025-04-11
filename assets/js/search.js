153e1234fa41c05e9d1fb9f4067aa034b3ea6048
Chirayu Desai <chirayudesai1@gmail.com>
2025-04-12 03:33:24 +0530

    Build
    6.6.20 manually excluded, merged too early
    2a579aaf fixup! posts: 6.5.21
    641ca00f fixup! posts: 6.5.21
    
    Change-Id: Iaa7088e9a8b9cac88cd5a4668cd780cb9b637a2e

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1744237948.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1744408973.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1744237948.json";
+    "/assets/sitecontents-1744408973.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
