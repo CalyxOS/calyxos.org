3478fbdf80ec2bca799cdf562dbdc0a138793e9a
Chirayu Desai <chirayudesai1@gmail.com>
2025-04-18 20:00:25 +0530

    Build
    dba2a074 device-flasher: 1.1.1
    2d4352e4 device-flasher: No more adb
    
    Change-Id: Ia900bdb6f48d302e3d279b517b0c277e2ff0ab1d

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1744984985.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1744986610.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1744984985.json";
+    "/assets/sitecontents-1744986610.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
