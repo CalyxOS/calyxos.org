18b9005cefe2e3993ca34dbfc1b7951d22fcf712
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-01 05:37:34 +0530

    Build
    c622d6d5 downloads: 6.6.23 beta security-express
    fc368a74 fixup! fixup! posts: 6.6.22 - now 6.6.23
    4767828e fixup! posts: 6.6.22 - now 6.6.23
    
    Change-Id: Ifd75d35154164aaa00627ebaed088ff7c98f4d2f

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1745627140.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1746058028.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1745627140.json";
+    "/assets/sitecontents-1746058028.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
