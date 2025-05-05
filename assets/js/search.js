f4b9836467f0a3c9525abb13450ae31e9e16ee0f
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-06 03:23:30 +0530

    Build
    4322b704 bundle update
    2a3eedea install: 6.6.23
    aa582765 downloads: 6.6.23 stable, factory
    
    Change-Id: I48c38df23b4af6a4c57ce1ef55fbd3b6e99b0b9b

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1746130168.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1746482000.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1746130168.json";
+    "/assets/sitecontents-1746482000.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
