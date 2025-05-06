146cfe0efb05686176b978d385dc990a2d03f2db
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-07 03:12:01 +0530

    Build: posts: Cleartext restriction
    Issue: calyxos#2987
    Co-Authored-By: Aysha <aysha@calyxinstitute.org>
    Co-Authored-By: Sage Cheng <sagecheng@calyxinstitute.org>
    Change-Id: I4f7272595491d2c39e17016157a8d573a02b121f

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1746482000.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1746567671.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1746482000.json";
+    "/assets/sitecontents-1746567671.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
