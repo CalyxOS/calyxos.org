06145f3d51524c35689ffb61e08d825f6746ff22
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-08 04:34:12 +0530

    Build
    7891c4fa install: 6.7.20 for 6,8 due to bootloader anti roll back
    7e6afd0d downloads: 6.7.20 SE, factory for 6,8
    3d0eb349 posts: 6.7.20
    
    Change-Id: I835d79da575d27c84f67b089770c8fa0c75290a5

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1746567671.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1746659046.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1746567671.json";
+    "/assets/sitecontents-1746659046.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
