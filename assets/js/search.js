b7859096d56b680c9d622aac714bdc18e693e2d0
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-13 03:19:18 +0530

    Build
    28833806 FP5: Pull build for rollback
    a199a4fc downloads,install: RiP 3-4a
    
    Change-Id: Id42bf9e1a4e35bc24210386bc2612af46d13b945

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1747065862.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1747086555.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1747065862.json";
+    "/assets/sitecontents-1747086555.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
