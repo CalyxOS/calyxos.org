aedd00619dbe6694cc3e88b8922fd8559dd3b98a
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-12 21:35:06 +0530

    Build
    84174c39 install: 6.7.20, 6.7.21-2
    6d2c89af downloads: 6.7.20, 6.7.21-2
    
    Change-Id: I9e3f780f2a3d0dea3737023679b9a15776a99751

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1746816528.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1747065862.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1746816528.json";
+    "/assets/sitecontents-1747065862.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
