572cc6633671d08d3a29ccbf672cb28a38204d8b
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-21 02:37:03 +0530

    update-releases: Drop fifteen from channels
    Change-Id: Ida2443b893fb276f88a5e2946c268bd60fa3f236

--- tasks/update-releases.rb
+++ tasks/update-releases.rb
@@ -5,7 +5,7 @@ require 'yaml'
 
 module Releases
 
-  RELEASES        = ["stable", "beta", "security-express", "factory", "fifteen"]
+  RELEASES        = ["stable", "beta", "security-express", "factory"]
   RELEASE_SOURCE  = 'https://review.calyxos.org/CalyxOS/release'
   HASHES_SOURCE   = 'https://review.calyxos.org/CalyxOS/releases'
   RELEASE_DL_BASE = 'https://release.calyxinstitute.org/'
