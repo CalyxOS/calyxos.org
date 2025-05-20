921eabff1b9ec7582701d8a79f51a3b9cab79f35
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-21 02:38:12 +0530

    Build
    c1c26b3c fixup! downloads: 6.7.22-2
    572cc663 update-releases: Drop fifteen from channels
    
    Change-Id: Ide34a205bc3ec1537f3e4c1602de7158475ac1c2

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1747774938.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1747775288.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1747774938.json";
+    "/assets/sitecontents-1747775288.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
