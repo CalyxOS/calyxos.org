9c39dd6cdacf977cc1dd9a72b948f517ff3c17f2
Chirayu Desai <chirayudesai1@gmail.com>
2025-05-30 00:10:24 +0530

    Build
    afc6d94c posts: moto blair
    a3282cf5 security_notes: Re-order devices per standard format
    3c0fb23c security_notes: new motos
    20893508 android14 -> android15-qpr2
    
    Change-Id: Ie0d7c3f1899012b043dd99055fc2689cb520b1b2

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1748456235.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1748544018.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1748456235.json";
+    "/assets/sitecontents-1748544018.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
