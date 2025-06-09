a70a6c0a93ba2b660a998e90ef7a936df6d6e71f
Chirayu Desai <chirayudesai1@gmail.com>
2025-06-10 01:34:42 +0530

    Build
    2af70aaa fixup! posts: 6.8.20, 6.8.21
    8bed19cc security_notes: Mention 9a
    
    Change-Id: Iba4c59de916ff8a0e879ec931d0d539cee4a8b66

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1749498780.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1749499470.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1749498780.json";
+    "/assets/sitecontents-1749499470.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
