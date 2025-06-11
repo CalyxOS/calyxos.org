eee5aad1778fa7fbc85f816d9bebb2d1c88a38db
Chirayu Desai <chirayudesai1@gmail.com>
2025-06-11 23:05:31 +0530

    Build
    bb3666f1 posts: Baklava plans
    f225e5da fixup! Move moto sm6225 devices to extended support
    
    Change-Id: Ia2f4417ff4424db2586c5a28982bbcfe2e532693

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1749502670.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1749663309.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1749502670.json";
+    "/assets/sitecontents-1749663309.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
