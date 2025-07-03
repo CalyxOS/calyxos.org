f60348446f4f98ee1b30d489c445933801ad1b7e
Tommy Webb <tommy@calyxinstitute.org>
2025-07-02 20:35:50 -0400

    Build commit 1e48205f8db52e7714acff01c3de0e00ba391b40
    Change-Id: I19ffa9ec3ec3c105bc24920e01a62dbaa79d9329

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1750973120.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1751503879.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1750973120.json";
+    "/assets/sitecontents-1751503879.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
