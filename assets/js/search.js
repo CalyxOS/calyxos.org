56ee2a1c31c5a51029f5fa6b0b8bd924b3fbe57e
Tommy Webb <tommy@calyxinstitute.org>
2025-08-05 20:50:23 -0400

    Build commit 2509b19f25854f4f8fc8603752b6735ae85a86aa
    1. Set commit variable to the commit to grab from Gerrit, e.g.
        `commit=57fe5e26b3d8344632e679519a3c27b2a6394d0b`
    2. Check out the main branch and the public branch in separate folders
        or worktrees.
        ```
        git clone https://gitlab.com/CalyxOS/calyxos.org.git --branch main calyxos.org-main
        git clone https://gitlab.com/CalyxOS/calyxos.org.git --branch public calyxos.org-public
        ```
    3. In the main tree, run (you can use docker if you don't have podman):
        ```
        cd calyxos.org-main
        git fetch https://review.calyxos.org/CalyxOS/calyxos.org $commit && git checkout FETCH_HEAD
        podman build -t calyxos.org-dev . && container=$(podman run --name calyxos.org-dev --replace --rm -d -p 8080:80 calyxos.org-dev:latest)
        ```
    4. Optionally test the site at http://localhost:8080
    5. Visit the public branch work tree and remove its contents:
        ```
        cd ../calyxos.org-public && rm -rf *
        ```
    6. Copy the output from the container and move/remove its contents appropriately:
        ```
        podman cp $container:/var/www/calyxos.org .
        mv calyxos.org/* .
        rmdir calyxos.org
        rm -rf data # untracked folder
        ```
    7. Prepare to commit and do so, potentially updating the commit message:
        ```
        git add .
        git commit -m "Build commit $commit" --edit
        ```
    8. Stop the container: `podman stop calyxos.org-dev`
    
    Change-Id: I17f15d834b2e5e5bbfbeeacabd470268d6286adb

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1754082804.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1754441131.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1754082804.json";
+    "/assets/sitecontents-1754441131.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
