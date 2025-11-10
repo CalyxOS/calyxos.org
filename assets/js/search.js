ff0d86ec1facbc510e500bdf542d60a643d2e226
Sage Cheng <sagecheng@calyxinstitute.org>
2025-11-10 16:35:00 -0500

    Build commit 8f9328028cbc282a491fd2972d9a5b56668d493c
    Change-Id: I1f0265e62f9a037e03707bfd2d56e3c9a558a6b5

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1762805777.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1762810178.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1762805777.json";
+    "/assets/sitecontents-1762810178.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
