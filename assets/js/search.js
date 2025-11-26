331fddb42d7d72672319deadbc4407f52e29d60a
SCheee <sage@calyx.org>
2025-11-26 13:18:52 -0500

    Build commit 0c04b941b284dd245ff347c3b6e922dc8d0448ca
    Change-Id: I345ed11b5096811664655a987d0d1494050e9321

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,9 +33,9 @@
   // get variables from jekyll
   // 
   // 
-  const SITE_CONTENTS_FILE = "sitecontents-1764086381.json";
+  const SITE_CONTENTS_FILE = "sitecontents-1764180734.json";
   const SITE_CONTENTS_URL =
-    "/assets/sitecontents-1764086381.json";
+    "/assets/sitecontents-1764180734.json";
 
   async function fetchDocuments() {
     if (window.localStorage) {
